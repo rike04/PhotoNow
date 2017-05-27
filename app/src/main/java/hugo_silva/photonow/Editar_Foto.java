ceb0f2c4f3987d27d1402a36ab9352a4ab9ea8b6
Hugo Silva <hugo_rfs10@hotmail.com>
2017-05-27 21:49:04 +0100

    editar foto xml feito

---
+++ app/src/main/java/hugo_silva/photonow/Editar_Foto.java
@@ -0,0 +1,13 @@
+package hugo_silva.photonow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Editar_Foto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar__foto);
+    }
+}
