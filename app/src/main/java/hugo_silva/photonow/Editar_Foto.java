b0350a13dc09ee9082bff6eb7ec7d7559a08fbe0
rike04 <rike0004@gmail.com>
2017-06-02 03:30:50 +0100

    Funcionalidade de encomenda

--- app/src/main/java/hugo_silva/photonow/Editar_Foto.java
+++ app/src/main/java/hugo_silva/photonow/Editar_Foto.java
@@ -4,9 +4,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +19,7 @@ import java.text.SimpleDateFormat;
 public class Editar_Foto extends Fragment {
 
     private Fotografia fotografia;
+    private EditText textLocal, textData, descr;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,16 +37,51 @@ public class Editar_Foto extends Fragment {
         Bitmap imagem = BitmapFactory.decodeFile(fotografia.getImagePath());
         viewFoto.setImageBitmap(Util.bitmapResizer(imagem, 320, 240));
 
-        TextView textLocal = (TextView) v.findViewById(R.id.text_view_local);
-        textLocal.setText(fotografia.getLocalizacao());
+        textLocal = (EditText) v.findViewById(R.id.text_view_local);
+        if(fotografia.getLocalizacao() != null) {
+            textLocal.setText(fotografia.getLocalizacao());
+        }
 
         Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String s = formatter.format(fotografia.getDataUpload());
-        TextView textData = (TextView) v.findViewById(R.id.text_view_data);
-        textLocal.setText(s);
+        textData = (EditText) v.findViewById(R.id.text_view_data);
+        textData.setText(s);
 
+        descr = (EditText) v.findViewById(R.id.text_view_descricao);
+        if(fotografia.getDescricao() != null) {
+            descr.setText(fotografia.getDescricao());
+        }
+
+        Button backButton = (Button) v.findViewById(R.id.botao_undo_editar);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
+
+        final Button concluirButton = (Button) v.findViewById(R.id.botao_concluir);
+        concluirButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                concluirEdicao();
+            }
+        });
 
         return v;
+    }
+
+    private void concluirEdicao() {
+        if(descr.getText() != null && descr.getText().length() > 0) {
+            fotografia.setDescricao(descr.getText().toString());
+        }
+
+        if(textLocal.getText() != null && textLocal.getText().length() > 0) {
+            fotografia.setLocalizacao(textLocal.getText().toString());
+        }
+
+        getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        Util.changeFragments(this, R.id.main_container, new GaleriaFragment());
     }
 
 
